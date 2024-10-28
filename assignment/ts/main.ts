786f2ca2fe8e269ef7be4da33982457380eecd67
default <mohamad.tanbari@mths.ca>
2024-10-28 03:35:00 +0000

    assignment 2 typescript

---
+++ assignment/ts/main.ts
@@ -0,0 +1,55 @@
+/*
+ * This program expands strings.
+ *
+ * Author: Mohamad Tanbari
+ * Version: 1.0.0
+ * Since: 2024-10-25
+ */
+
+function blowUp (string: string): string {
+  let stringCopy = string // Copy of the inputted string, to not modify original
+  let newString: string = '' // The new string that will be returned
+
+  while (stringCopy.length > 0) {
+    // Variables
+    let cachedNumber: number = 1
+    let cachedLetter: string = ''
+    const isLargeEnough: boolean = stringCopy.length > 1
+
+    // Check if there is a number followed by a letter
+    const tempNumber = parseInt(stringCopy[0])
+    // Only check the following number if the string is large enough
+    let tempNumber2: number = -1
+    if (isLargeEnough) {
+      tempNumber2 = parseInt(stringCopy[1])
+    }
+
+    if (tempNumber2 !== -1 && !isNaN(tempNumber) && isNaN(tempNumber2)) {
+      // if there is a number followed by a letter
+      // save the to cache letters
+      cachedNumber = tempNumber
+      cachedLetter = stringCopy[1]
+
+      // remove the letters from the string
+      stringCopy = stringCopy.slice(0, 0) +
+        stringCopy.slice(2)
+    } else {
+      // If the next two characters don't fit the format
+      newString = newString += stringCopy[0]
+      stringCopy = stringCopy.slice(0, 0) +
+        stringCopy.slice(1)
+    }
+
+    // Add the letter x times depending on the cached number
+    for (let counter = 0; counter < cachedNumber; counter++) {
+      newString += cachedLetter
+    }
+  }
+
+  return newString
+}
+
+const testString: string = '2xy5a55'
+
+console.log(`Original string: ${testString}`)
+console.log(`Blown up string: ${blowUp(testString)}`)

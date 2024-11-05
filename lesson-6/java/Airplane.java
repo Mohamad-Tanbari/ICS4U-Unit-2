3d20d383ed959fc35929be5f3fa907f67ef3ebb7
default <mohamad.tanbari@mths.ca>
2024-11-05 18:29:57 +0000

    a

---
+++ lesson-6/java/Airplane.java
@@ -0,0 +1,40 @@
+/*
+ * The airplane parent class
+ *
+ * @author Mohamad Tanbari
+ * @version 1.0.0
+ * @since 2024-11-05
+ */
+
+/**
+ * The airplane class
+ */
+public class Airplane {
+    /**
+     * The speed property.
+     */
+    protected int speed;
+
+    /**
+     * Constructor.
+     */
+    public Airplane() { }
+
+    /**
+     * The speed setter.
+     *
+     * @param speed The speed to set the airplane to.
+     */
+    public void setSpeed(int speed) {
+      this.speed = speed;
+    }
+
+    /**
+     * The speed getter.
+     *
+     * @return Speed as an integer.
+     */
+    public int getSpeed() {
+      return this.speed;
+    }
+}

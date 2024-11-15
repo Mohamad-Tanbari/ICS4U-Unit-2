e7a3a047204640c83b6e26295620aabe9ab47f70
Mohamad-Tanbari <124389317+Mohamad-Tanbari@users.noreply.github.com>
2024-11-15 09:12:25 -0500

    asd

---
+++ lesson-9/java/Truck.java
@@ -0,0 +1,83 @@
+/*
+ * This is the truck class
+ *
+ * @author Mohamad Tanbari
+ * @version 1.0.0
+ * @since 2024-11-13
+ */
+
+/**
+ * The Truck class.
+ */
+public class Truck extends Vehicle {
+    /**
+     * The liscense plate orf the vehicle.
+     */
+    private String liscensePlate;
+
+    /**
+     * The number of doors the vehicle has.
+     */
+    private final int numberOfDoors = 2;
+
+    /**
+     * Constructor.
+     *
+     * @param color - The color you want the vehicle to start with.
+     * @param liscensePlate - The liscense plate you want the vehicle to have.
+     * @param maxSpeed - The max speed the vehicle can reach.
+     * @param numberOfWheels - The number of wheels the truck will have.
+     */
+    public Truck(
+        String color,
+        String liscensePlate,
+        int maxSpeed,
+        int numberOfWheels) {
+        // Set the properties
+        super(color, maxSpeed, numberOfWheels);
+        this.liscensePlate = liscensePlate;
+    }
+
+    /**
+     * The getter for the liscensePlate property.
+     *
+     * @return a string containing the liscense plate.
+     */
+    public String getLiscensePlate() {
+        return this.liscensePlate;
+    }
+
+    /**
+     * The setter for the liscensePlate property.
+     *
+     * @param newLiscensePlate - The new liscense plate you want to assign.
+     */
+    public void setLiscensePlate(String newLiscensePlate) {
+        this.liscensePlate = newLiscensePlate;
+    }
+
+    /**
+     * The status getter to show info about all properties of the vehicle.
+     *
+     * @return A string containing all the properties.
+     */
+    public String status() {
+        final String status = "\n-> Speed: " + super.getSpeed()
+                            + "\n-> Max Speed: " + super.getMaxSpeed()
+                            + "\n-> Number of Doors: " + this.numberOfDoors
+                            + "\n-> Liscense Plate: " + this.liscensePlate
+                            + "\n-> Wheels: " + super.getNumberOfWheels()
+                            + "\n-> Color: " + super.getColor();
+
+        return status;
+    }
+
+    /**
+     * The provideAir method.
+     *
+     * @param airPressure - The air pressure to apply.
+     */
+    public void provideAir(int airPressure) {
+        super.setSpeed(super.getSpeed() - airPressure / 2);
+    }
+}
